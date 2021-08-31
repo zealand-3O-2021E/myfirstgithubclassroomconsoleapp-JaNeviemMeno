37ef2dca21ede98851e1d2e8e3ea72b65e0d3d97
JaNeviemMeno <75016040+JaNeviemMeno@users.noreply.github.com>
2021-08-31 14:25:46 +0200

    Car class added

---
+++ MyFirstGitHubClassRoomConsoleApp/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    public class Car
+    {
+        private int _wheels;
+        private int _drivers;
+        private string _number;
+
+        public Car(int wheels,int drivers, string number)
+        {
+            _wheels = wheels;
+            _drivers = drivers;
+            _number = number;
+        }
+
+
+        public int Wheels
+        {
+            get;set;
+        }
+        public int Drivers
+        {
+            get; set;
+        }
+        public int Numbers
+        {
+            get; set;
+        }
+    }
+}
